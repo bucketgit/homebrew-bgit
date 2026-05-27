53f257a1f0f664d9f1e29e12fa2f24eaa47b54cf
Dennis Vink <dennis.vink@xebia.com>
2026-05-27 16:19:22 +0200

    Bumped version to 1.2.0

--- Formula/bgit.rb
+++ Formula/bgit.rb
@@ -1,7 +1,7 @@
 class Bgit < Formula
   desc "Git CLI for repositories stored directly in GCS or S3 buckets"
   homepage "https://bucketgit.com/"
-  version "1.1.3"
+  version "1.2.0"
   license "MIT"
 
   release_tag = version.to_s
@@ -9,24 +9,24 @@ class Bgit < Formula
   on_macos do
     on_arm do
       url "https://github.com/bucketgit/bgit/releases/download/#{release_tag}/bgit-mac-arm64"
-      sha256 "8de03e41f4699f3e4c2d048f4ac446cdb6c2fd6bfe18513ff440201dc14efcab"
+      sha256 "5b41ac96ff22ac5556eeae8555fb800fac635cc21669707b8961b909e63c7e9c"
     end
 
     on_intel do
       url "https://github.com/bucketgit/bgit/releases/download/#{release_tag}/bgit-mac-amd64"
-      sha256 "9872edbcf4a5605ee1cd772b791a8c0b0b2c020ab406c774e39e0dbd64f9bcfa"
+      sha256 "ca27331b63f466b0f9cffcd88b406bea9ab614138bcf39e1b74b6e07244cc5be"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/bucketgit/bgit/releases/download/#{release_tag}/bgit-linux-amd64"
-      sha256 "afbe27ede68e671ed9e6c076a7ecda155f38a042ad9a8d36ebd4605ceafa3a57"
+      sha256 "85e98f09de7279c979957a2f0a1a74584c012d73ca6aed2dd06fe2fdcc925ba9"
     end
 
     on_arm do
       url "https://github.com/bucketgit/bgit/releases/download/#{release_tag}/bgit-linux-arm64"
-      sha256 "ed6c30fb810a4c6f27daa4178bb1e068b12d44a1119d579ca7477fe8fdc3097b"
+      sha256 "64269a1d2a680981482cb61249aac25a4d31d5428e4e112fe859169b1b3e79ee"
     end
   end
 
